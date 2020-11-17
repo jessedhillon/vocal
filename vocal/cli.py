0180ce61974f0e047eb412334d9b062af0decfda
Jesse Dhillon <jesse@dhillon.com>
2020-11-17 10:46:57 -0800

    feat(database): add alembic config, commands to cli

--- vocal/cli.py
+++ vocal/cli.py
@@ -6,7 +6,9 @@ from pathlib import Path
 from importlib import import_module
 
 import aiohttp.web
+import alembic.config
 import click
+import sqlalchemy
 
 import vocal.config
 import vocal.log
@@ -59,3 +61,42 @@ def serve(config, app_path):
     logger.info(f"starting {appname}")
 
     aiohttp.web.run_app(app, **appconf)
+
+
+@main.group()
+@click.pass_obj
+def database(config):
+    dbconf = config['database']
+
+    connargs = dbconf['connection']
+    connargs.update(config['secrets']['database'])
+    dsn = sqlalchemy.engine.url.URL('postgresql', **connargs)
+    escaped = str(dsn).replace('%', '%%')
+
+    alconf = alembic.config.Config()
+    alconf.set_main_option('script_location', str(base_path / 'vocal' / 'migrations'))
+    alconf.set_section_option('alembic', 'sqlalchemy.url', escaped)
+    alconf.set_section_option('alembic', 'file_template', '%%(slug)s-%%(rev)s')
+
+    config['alembic'] = alconf
+
+
+@database.command()
+@click.argument('revision')
+@click.pass_obj
+def up(config, revision):
+    alembic.command.upgrade(config.pop('alembic'), revision)
+
+
+@database.command()
+@click.argument('revision')
+@click.pass_obj
+def down(config, revision):
+    alembic.command.downgrade(config.pop('alembic'), revision)
+
+
+@database.command()
+@click.argument('message')
+@click.pass_obj
+def generate(config, message):
+    alembic.command.revision(config.pop('alembic'), message)
