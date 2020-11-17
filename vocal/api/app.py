75444f9c36805c1d326ba693f8cf12af8cbf000a
Jesse Dhillon <jesse@dhillon.com>
2020-11-17 08:15:32 -0800

    feat(session): add session configuration
    Use aiohttp_session configured with redis backend to add session support

--- vocal/api/app.py
+++ vocal/api/app.py
@@ -1,3 +1,6 @@
+import aioredis
+import aiohttp_session
+from aiohttp_session.redis_storage import RedisStorage
 from aiohttp.web import Application
 
 from .routes import routes
@@ -5,12 +8,23 @@ from .routes import routes
 
 async def configure(config):
     # e.g. configure storage, template renderer, etc
-    return {
-        'routes': routes
+    conf = {
+        'routes': routes,
     }
+
+    sc = config.get('session')
+    if sc is not None:
+        if sc['storage'] == 'redis':
+            pool = await aioredis.create_pool(sc['pool'])
+            conf['session_store'] = RedisStorage(pool, cookie_name=sc.get('cookie_name'))
+        else:
+            raise ValueError(f"unsupported session storage: {sc['storage']}")
+
+    return conf
 
 
 async def initialize(config):
     app = Application()
     app.add_routes(config['routes'])
+    aiohttp_session.setup(app, config['session_store'])
     return app
