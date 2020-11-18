6a473c3017d586be7d0fb49875b7dd3500305ab9
Jesse Dhillon <jesse@dhillon.com>
2020-11-18 14:17:29 -0800

    refactor(config): reorient configuration to be app/env

--- vocal/cli.py
+++ vocal/cli.py
@@ -16,23 +16,29 @@ from vocal.util.asyncio import synchronously
 
 
 base_path = Path(__file__).parent.parent
-config_path = str(base_path / 'config')
-
 
 @click.group()
 @click.option('-D', '--debug', is_flag=True, default=False)
-@click.option('-C', '--config_path', default=config_path)
+@click.option('-C', '--config_path', default=None)
+@click.option('-E', '--env', default='dev')
+@click.argument('app_path')
 @click.pass_context
 @synchronously(pass_loop=True)
-async def main(loop, ctx, debug, config_path):
+async def main(loop, ctx, debug, config_path, env, app_path):
     try:
         loop.set_debug(debug)
+
+        if config_path is None:
+            config_path = base_path / 'config' / app_path
         if ctx.obj is None:
             ctx.obj = {}
-        ctx.obj.update(await vocal.config.load_config(config_path))
-        await vocal.log.configure(ctx.obj)
 
+        ctx.obj.update(await vocal.config.load_config(config_path, env))
+        ctx.obj['module'] = import_module(app_path)
+
+        await vocal.log.configure(ctx.obj)
         logger = logging.getLogger(__name__)
+
         if debug:
             ctx.obj['debug'] = True
             logger.info('debugging mode enabled')
@@ -44,22 +50,21 @@ async def main(loop, ctx, debug, config_path):
 
 
 @main.command()
-@click.argument('app_path')
 @click.pass_obj
-def serve(config, app_path):
+def serve(config):
     loop = asyncio.get_event_loop()
     logger = logging.getLogger(__name__)
-    mod = import_module(app_path)
+    mod = config['module']
 
     async def setup_app(config, module):
         config.update(await mod.configure(config))
         return await module.initialize(config)
 
     app = loop.run_until_complete(setup_app(config, mod))
-    appconf = config[app_path]
+    appconf = config['app'].copy()
     appname = appconf.pop('name')
-    logger.info(f"starting {appname}")
 
+    logger.info(f"starting {appname}")
     aiohttp.web.run_app(app, **appconf)
 
 
--- vocal/config.py
+++ vocal/config.py
@@ -3,19 +3,28 @@ from pathlib import Path
 import yaml
 
 
-async def load_config(config_path):
-    cfpath = Path(config_path) / 'config.yaml'
-    with cfpath.open('r') as cf:
-        config = yaml.load(cf.read(), Loader=yaml.CLoader) or {}
+async def load_config(config_path, env):
+    env_path = Path(config_path) / env
+    common_path = Path(config_path) / 'common'
 
-    for p in cfpath.parent.glob('*.yaml'):
+    base_conf = {}
+    if common_path.exists():
+        for p in common_path.parent.glob('*.yaml'):
+            path = Path(p)
+            keyname = path.stem
+            if keyname in base_conf:
+                raise RuntimeError(f"disallowed or multiply-defined configuration key: common/{keyname}")
+            with path.open('r') as f:
+                base_conf[keyname] = yaml.load(f.read(), Loader=yaml.CLoader)
+
+    env_conf = {}
+    for p in env_path.glob('*.yaml'):
         path = Path(p)
-        if path.samefile(cfpath):
-            continue
         keyname = path.stem
-        if keyname in config:
-            raise RuntimeError(f"disallowed or multiply-defined configuration key: {keyname}")
+        if keyname in env_conf:
+            raise RuntimeError(f"disallowed or multiply-defined configuration key: {env}/{keyname}")
         with path.open('r') as f:
-            config[keyname] = yaml.load(f.read(), Loader=yaml.CLoader)
+            env_conf[keyname] = yaml.load(f.read(), Loader=yaml.CLoader)
 
-    return config
+    base_conf.update(env_conf)
+    return base_conf
